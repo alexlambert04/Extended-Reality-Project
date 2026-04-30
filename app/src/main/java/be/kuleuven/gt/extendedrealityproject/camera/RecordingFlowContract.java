6d9cc745820ffd58c790e4f4479d8c4e9e757249
laurens <laurens.pirard@student.kuleuven.be>
2026-04-30 15:08:51 +0200

    gif instruction before start recording

--- app/src/main/java/be/kuleuven/gt/extendedrealityproject/camera/RecordingFlowContract.java
+++ app/src/main/java/be/kuleuven/gt/extendedrealityproject/camera/RecordingFlowContract.java
@@ -2,8 +2,8 @@ package be.kuleuven.gt.extendedrealityproject.camera;
 
 public final class RecordingFlowContract {
 
-    public static final long MAX_RECORDING_MS = 180_000L;
-    public static final long RECOMMENDATION_MS = 30_000L;
+    public static final long MAX_RECORDING_MS = 30_000L;
+    public static final long RECOMMENDATION_MS = 31_000L; // disabled – max is 30 s
     public static final long MAX_UPLOAD_BYTES = 50L * 1024L * 1024L;
     public static final long RECOMMENDED_UPLOAD_BYTES = 45L * 1024L * 1024L;
 
